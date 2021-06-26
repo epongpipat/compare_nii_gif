bf66e0d3c99155b2b7805592c91467ef2cd4e10a
epongpipat <epongpiat@gmail.com>
2021-06-26 02:30:23 -0500

    update setup with version and install requirements

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
 setup(name='compare_nii_gif',
-      version='0.1',
+      version='0.1.0',
       description='Create a gif that compares nifti files in axial, sagittal, and coronal views',
       url='',
       author='Ekarin E. Pongpipat',
       author_email='epongpipat@gmail.com',
       license='MIT',
       packages=['compare_nii_gif'],
+      install_requires=['nilearn', 'numpy', 'imageio'],
       entry_points={'console_scripts': [
           'compare_nii_gif = compare_nii_gif.__main__:main']},
       zip_safe=False)
